21f2e329d064c5ad99835d18f705751a0209e4e5
faza-pratama <alinnopratama@gmail.com>
2025-10-22 17:24:47 +0700

    perubahan 2

--- SimpleProgram/src/Main.java
+++ SimpleProgram/src/Main.java
@@ -3,17 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        /**
+         * @param a nilai a
+         * @param b nilai b
+         */
 
         System.out.print("masukan nilai a:");
         int a = scanner.nextInt();
 
         System.out.print("masukan nilai b:");
         int b = scanner.nextInt();
-
-        /**
-         * @param a nilai a
-         * @param b nilai b
-         */
 
         System.out.println("ini a=" + a + " " + "ini b=" + b);
 
